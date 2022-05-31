2e0c058d81bbd7d3366013eb1875338ef13f5f07
Mukiti-Ryan <87067381+Mukiti-Ryan@users.noreply.github.com>
2022-05-31 10:44:25 +0300

    Initial commit

--- src/main/java/com/ticketreservation/eventticketreservation/EventReservationApplication.java
+++ src/main/java/com/ticketreservation/eventticketreservation/EventReservationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventReservationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventReservationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventReservationApplication.class, args);
+    }
 
 }
--- src/test/java/com/ticketreservation/eventticketreservation/EventReservationApplicationTests.java
+++ src/test/java/com/ticketreservation/eventticketreservation/EventReservationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EventReservationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
